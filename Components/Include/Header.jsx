bc0bb48f09af3fd9c9b225af7b5978659c47c561
Shubham Rana <s4shubham.rana20@gmail.com>
2024-03-14 10:39:09 +0530

    Initialize project using Create React App

---
+++ Components/Include/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <>
+        <h1>Header Part</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contactus</li>
+            </ul>
+        </nav>
+    </>
+  )
+}
